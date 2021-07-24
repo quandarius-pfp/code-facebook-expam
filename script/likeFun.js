8eca056cfecdffb6a19bd591794c140354743f43
quandarius-pfp <78605946+quandarius-pfp@users.noreply.github.com>
2021-07-24 23:31:44 +0700

    oke

---
+++ script/likeFun.js
@@ -0,0 +1,10 @@
+function likeFun(x){
+    x.style.fontWeight = "both";
+    x.innerHTML = " Chấp nhận";
+    x.style.backgroundColor='red';
+}
+function dontlike(y){
+    y.style.fontWeight="both";
+    y.innerHTML="Đã Xóa";
+    y.style.backgroundColor='rgba(255,255,255,0.1)';
+}
